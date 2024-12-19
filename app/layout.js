91f9fb6b73f0d426e4b943cbc41ef0a3d20fec97
n8 ô¿ô <natewbrooks@gmail.com>
2024-12-19 15:24:38 -0500

    Added favicon

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,9 @@ const acme = Acme({ subsets: ['latin'], weight: ['400'] });
 export const metadata = {
 	title: 'natewbrooks',
 	description: 'portfolio',
+	icons: {
+		icon: '/nwb-favicon.svg',
+	},
 };
 
 export default function RootLayout({ children }) {
